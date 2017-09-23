10785c71ba6b6c7e51cf2c59ed8782a196360ddc
Marko Stijak <mstijak@gmail.com>
2017-09-23 23:10:47 +0200

    ...

---
+++ app/components/OnScreenLoader.js
@@ -0,0 +1,78 @@
+import {PureContainer} from 'cx/widgets';
+import {VDOM} from 'cx/ui';
+
+export class OnScreenLoader extends PureContainer {
+    initState(context, instance) {
+        instance.state = {active: !window.IntersectionObserver};
+    }
+
+    explore(context, instance) {
+        if (instance.state.active)
+            super.explore(context, instance);
+    }
+
+    prepare(context, instance) {
+        if (instance.state.active)
+            super.prepare(context, instance);
+    }
+
+    cleanup(context, instance) {
+        if (instance.state.active)
+            super.cleanup(context, instance);
+    }
+
+    render(context, instance, key) {
+        return <OnScreen key={key} instance={instance} data={instance.data}>
+            {instance.state.active && this.renderChildren(context, instance)}
+        </OnScreen>
+    }
+}
+
+OnScreenLoader.prototype.styled = true;
+
+class OnScreen extends VDOM.Component {
+    render() {
+        if (!this.elRef)
+            this.elRef = el => {
+                this.el = el
+            };
+
+        let {children, data} = this.props;
+        return <div
+            ref={this.elRef}
+            data={data.classNames}
+            style={data.style}
+        >
+            {children}
+        </div>
+    }
+
+    componentDidMount() {
+        if (window.IntersectionObserver) {
+            this.observer = new IntersectionObserver(::this.onIntersection, {
+                target: document.querySelector('.drawer')
+            });
+            this.observer.observe(this.el);
+        }
+    }
+
+    destroy() {
+        if (this.observer)
+            this.observer.disconnect();
+        delete this.observer;
+    }
+
+    componentWillUnmount() {
+        this.destroy();
+    }
+
+    onIntersection(entries) {
+        entries.forEach(e => {
+            if (e.isIntersecting) {
+                console.log('INTERSECTION', e);
+                this.props.instance.setState({active: true});
+                this.destroy();
+            }
+        })
+    }
+}
--- app/routes/home/index.js
+++ app/routes/home/index.js
@@ -1,25 +1,45 @@
-import { HtmlElement, LinkButton, Section, FlexRow } from "cx/widgets";
+import {HtmlElement, LinkButton, Link, Section, FlexRow} from "cx/widgets";
 
 import Controller from "./Controller";
-import { Repeater } from "cx/ui";
+import {Repeater} from "cx/ui";
 
 export default (
-	<cx>
-		<FlexRow controller={Controller} padding spacing>
-			<Section mod="card" style="margin: 10px" title="My Dashboards">
-				<Repeater records:bind="dashboards">
-					<LinkButton
-						mod="hollow"
-						style="display:block; padding: 20px; height: auto"
-						href:tpl="~/{$record.id}"
-						text:tpl="{$record.title}"
-					/>
-				</Repeater>
+    <cx>
+        <LinkButton href="~/new" mod="primary" putInto="tools">
+            New Dashboard
+        </LinkButton>
+        <FlexRow controller={Controller} padding spacing wrap>
+            <Section mod="card" style="margin: 10px" title="My Dashboards" hLevel={4}>
+                <div visible:expr="!!{user.id}">
+                    <Repeater records:bind="dashboards">
+                        <LinkButton
+                            mod="hollow"
+                            style="display:block; padding: 20px; height: auto"
+                            href:tpl="~/{$record.id}"
+                            text:tpl="{$record.title}"
+                        />
+                    </Repeater>
+                    <p visible:expr="!{dashboards.length}" ws>
+                        No dashboards in your inventory.
+                        <Link href="~/new">Create a new dashboard</Link>
+                    </p>
+                </div>
+                <p visible:expr="!{user.id}" ws>
+                    Please <Link href="~/sign-in">sign-in</Link> to see your dashboards.
+                </p>
+            </Section>
 
-				<LinkButton href="~/new" mod="primary" putInto="tools">
-					New Dashboard
-				</LinkButton>
-			</Section>
-		</FlexRow>
-	</cx>
+            <Section mod="card" style="margin: 10px" title="Sample Dashboards" hLevel={4}>
+
+                    <LinkButton
+                        mod="hollow"
+                        style="display:block; padding: 20px; height: auto"
+                        href="~/lao6cfj"
+                    >
+                        Crypto
+                    </LinkButton>
+
+            </Section>
+        </FlexRow>
+    </cx>
 );
