c1163a1fd75f212956082331e7db162be47c44aa
Ognjen1994 <ognjen.risti@gmail.com>
2017-10-14 15:28:22 +0200

    Added dollar-to-euro widget.

---
+++ app/widgets/dollar-to-euro/index.js
@@ -0,0 +1,25 @@
+import {Rescope} from "cx/widgets";
+
+import Controller from "./Controller";
+import { enableCultureSensitiveFormatting } from "cx/ui";
+
+enableCultureSensitiveFormatting();
+
+const defaultProps = {};
+
+export default (props = defaultProps) =>
+	<cx>
+		<Rescope bind="$data">
+		<div class="kpi-header" controller={Controller}>
+			Dollar To Euro	
+		</div>
+		<div class="kpi-main">
+			<div class="kpi-value" text:tpl="$1 = &euro;{dollarToEuro}" />
+		</div>
+		<div class="kpi-footer">
+			<a href="http://fixer.io/" target="_blank" rel="noopener">
+				fixer.io
+			</a>
+		</div>
+		</Rescope>
+	</cx>;
