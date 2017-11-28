50f2bcc9c0037cca4f860316170d4cf55922a7b2
Marko Stijak <mstijak@gmail.com>
2017-11-28 14:35:29 +0100

    Fix typos, add links

--- app/routes/about/index.js
+++ app/routes/about/index.js
@@ -15,17 +15,19 @@ export default (
 			<Section mod="card" style="padding: 50px">
 				<h4>Info</h4>
 				<p ws>
-					Dashboards allow you to build personalized dashboards by arranging
-					widgets on a rectangular board. Each board can be customized to use
-					different background color or image. If you sign in your dashboards
-					will be saved.
+					Dashboards allows you to build personalized dashboards by arranging
+					widgets on a rectangular board. Each board can be customized with different size,
+					background image or color. Dashboards are stored in browser's local storage.
+					If you sign in your dashboards will be saved to Google Firebase.
 				</p>
 				<br />
 				<h4>Tech</h4>
 				<p ws>
-					The application is built using CxJS, React, Babel and webpack and uses
+					The application is built using <a href="https://cxjs.io">CxJS</a>,
+					<a href="https://reactjs.org/">React</a>, <a href="https://babeljs.io/">Babel</a> and
+					<a href="https://webpack.js.org/">webpack</a> and uses
 					not yet fully supported browser features such as CSS grid layout and
-					IntersectionObserver. Application source code is available on{" "}
+					IntersectionObserver. Application source code is available on
 					<a href="https://github.com/codaxy/dashboards">GitHub</a>. Developers
 					are encouraged to contribute new widgets and new ideas.
 				</p>
